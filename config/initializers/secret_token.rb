842536e93c6651ac9baa47801f8209472c870048
JC <jeff.cummings@atwoodgroup.ca>
2011-10-24 17:10:12 -0300

    This is the first time I'm adding files to GIT.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c135432079d16232056cfd8ffde5b7961058095610340390b85749afd5b0f238c1ec3b3c6caa00b3a17160dda4b3a26faad20cb83fb3a60ebbf5586a73e08d4a'
